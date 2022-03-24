527248f5f56421aa0db1c6e052565fe0225d497c
gabrielccsantos42 <gabrielccsantos42@gmail.com>
2022-03-24 13:24:11 -0300

    Criação das exceções

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,11 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 import model.entities.Account;
+import model.execptions.DomainExceptions;
 
 public class Program {
 
@@ -13,23 +15,31 @@ public class Program {
 		
 		Account acc;
 		
-		System.out.println("Entre com os dados da conta");
-		System.out.print("Número: ");
-		int numero = read.nextInt();
-		System.out.print("Nome: ");
-		String nome = read.nextLine();
-		System.out.print("Saldo Inicial: ");
-		double saldoInicial = read.nextDouble();
-		System.out.print("Limite do saque: ");
-		double limiteSaque = read.nextDouble();
-		
-		acc = new Account(numero, nome, saldoInicial, limiteSaque);
-		
-		System.out.println("Qual valor do saque?");
-		acc.saque(read.nextDouble());
-		
-		System.out.println(acc);
-		
+		try {
+			System.out.println("Entre com os dados da conta");
+			System.out.print("Número: ");
+			int numero = read.nextInt();
+			System.out.print("Nome: ");
+			read.nextLine();
+			String nome = read.nextLine();
+			System.out.print("Saldo Inicial: ");
+			double saldoInicial = read.nextDouble();
+			System.out.print("Limite do saque: ");
+			double limiteSaque = read.nextDouble();
+			
+			acc = new Account(numero, nome, saldoInicial, limiteSaque);
+			
+			System.out.println("Qual valor do saque?");
+			acc.saque(read.nextDouble());
+			
+			System.out.println(acc);
+		}
+		catch(InputMismatchException e) {
+			System.out.println("Valor digitado errado!");
+		}
+		catch(DomainExceptions e) {
+			System.out.println(e.getMessage());
+		}
 		read.close();
 	}
 
--- src/model/entities/Account.java
+++ src/model/entities/Account.java
@@ -1,17 +1,19 @@
 package model.entities;
 
+import model.execptions.DomainExceptions;
+
 public class Account {
 	private Integer numero;
 	private String nome;
 	private Double saldo;
-	private Double withdrawLimit;
+	private Double limiteSaque;
 	
-	public Account(Integer numero, String nome, Double saldo, Double withdrawLimit) {
+	public Account(Integer numero, String nome, Double saldo, Double limiteSaque) {
 		super();
 		this.numero = numero;
 		this.nome = nome;
 		this.saldo = saldo;
-		this.withdrawLimit = withdrawLimit;
+		this.limiteSaque = limiteSaque;
 	}
 	
 	
@@ -46,8 +48,8 @@ public class Account {
 
 
 
-	public Double getWithdrawLimit() {
-		return withdrawLimit;
+	public Double getLimiteSaque() {
+		return limiteSaque;
 	}
 
 
@@ -56,10 +58,15 @@ public class Account {
 		this.saldo += deposito;
 	}
 	
-	public void saque(Double saque) {
+	public void saque(Double saque) throws DomainExceptions {
+		if(saque > limiteSaque) {
+			throw new DomainExceptions("Valor ultrapassou limte de saque");
+		}
+		if(saque > saldo) {
+			throw new DomainExceptions("Valor ultrapassou o saldo da conta");
+		}
 		this.saldo -= saque;
 	}
-	
 	
 	@Override
 	public String toString() {
